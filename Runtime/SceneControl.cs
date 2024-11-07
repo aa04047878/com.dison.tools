e5f4e8869c67b99098e08a1b45e1c67f269780ef
aa04047878 <aa04047878@gmail.com>
2024-11-07 11:05:49 +0800

    Initial commit

---
+++ Runtime/SceneControl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Dison.Tools
+{
+    /// <summary>
+    /// 場景控制(載入場景 & 退出遊戲)
+    /// </summary>
+    public class SceneControl : MonoBehaviour
+    {
+        /// <summary>
+        /// 載入場景
+        /// </summary>
+        /// <param name="sceneName">要載入的場景名子</param>
+        public void LoadScene(string sceneName)
+        {
+            SceneManager.LoadScene(sceneName);
+        }
+
+        /// <summary>
+        /// 退出遊戲
+        /// </summary>
+        public void QuitGame()
+        {
+            //離開遊戲
+            Application.Quit();
+        }
+
+
+
+    }
+}
+
